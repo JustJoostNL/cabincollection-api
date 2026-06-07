81ffcf77c53ef39d677eddfd1f020db7d8a5a603
Joost <git@jstt.me>
2026-06-07 19:02:05 +0200

    Add Expo auth plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
+import { expo } from "@better-auth/expo";
 import { passkey } from "@better-auth/passkey";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth/minimal";
@@ -7,11 +8,15 @@ export const auth = () => {
   const prisma = getPrismaClient();
 
   return betterAuth({
+    trustedOrigins: ["cabincollection://"],
     user: { modelName: "users" },
     session: { modelName: "sessions" },
     account: { modelName: "accounts" },
     verification: { modelName: "verifications" },
     database: prismaAdapter(prisma, { provider: "postgresql" }),
-    plugins: [passkey({ schema: { passkey: { modelName: "passkeys" } } })],
+    plugins: [
+      passkey({ schema: { passkey: { modelName: "passkeys" } } }),
+      expo(),
+    ],
   });
 };
